174d74ddb61550785c2fce137e63a5b38f7412b0
wtfiAndrewNelson <noslenwerdna@hotmail.com>
2022-09-25 08:49:09 -0400

    Add files via upload

--- lidar8.ino
+++ lidar8.ino
@@ -31,7 +31,7 @@ bool isScanning = false;
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
 
-#define OLED_Address 0x3c          //initialize with the I2C addr 0x3C
+#define OLED_Address 0x3c         // initialize with the I2C addr 0x3C
 #define SCREEN_WIDTH 128          // OLED display width, in pixels
 #define SCREEN_HEIGHT 64          // OLED display height, in pixels
 #define OLED_RESET -1             //
